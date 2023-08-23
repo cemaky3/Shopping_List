ad0da602b76e2465e4e6256bee0481a519c1cea6
cemaky3 <cemaky3>
2023-08-23 13:55:37 +0300

    ShopItemActivity Screen + ViewModel

---
+++ app/src/main/java/com/example/my_shopping_list/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package com.example.my_shopping_list.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.my_shopping_list.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
---
+++ app/src/main/java/com/example/my_shopping_list/presentation/ShopItemViewModel.kt
@@ -0,0 +1,86 @@
+package com.example.my_shopping_list.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.my_shopping_list.data.ShopListRepositoryImpl
+import com.example.my_shopping_list.domain.AddItemUseCase
+import com.example.my_shopping_list.domain.EditItemUseCase
+import com.example.my_shopping_list.domain.GetItemByIdUseCase
+import com.example.my_shopping_list.domain.ShopItem
+
+class ShopItemViewModel : ViewModel() {
+    private val shopListRepository = ShopListRepositoryImpl
+
+    private val addItemUC = AddItemUseCase(shopListRepository)
+    private val editItemUC = EditItemUseCase(shopListRepository)
+    private val getItemByIdUC = GetItemByIdUseCase(shopListRepository)
+
+    private val _errorInput = MutableLiveData<Boolean>()
+    val errorInput: LiveData<Boolean>
+        get() = _errorInput
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+        get() = _shouldCloseScreen
+
+    fun getShopItem(id: Int): ShopItem {
+        return getItemByIdUC.getItemById(id)
+    }
+
+    fun addItem(newName: String?, newCount: String?) {
+        val name = parseName(newName)
+        val count = parseCount(newCount)
+        val isValid = isValidInput(name, count)
+        if (isValid) {
+            val item = ShopItem(name, count, enabled = true)
+            addItemUC.addItem(item)
+            finishWork()
+        }
+    }
+
+    fun editItem(newName: String?, newCount: String?) {
+        val name = parseName(newName)
+        val count = parseCount(newCount)
+        val isValid = isValidInput(name, count)
+        if (isValid) {
+            val item = _shopItem
+            item.value?.let {
+                val newItem = it.copy(name = name, count = count)
+                editItemUC.editItem(newItem)
+                finishWork()
+            }
+        }
+    }
+
+    fun parseName(name: String?): String {
+        return name?.trim() ?: ""
+    }
+
+    fun parseCount(count: String?): Int {
+        return count?.trim()?.toIntOrNull() ?: 0
+    }
+
+    private fun isValidInput(name: String, count: Int): Boolean {
+        // TODO: show error message
+        if (name.isNotBlank() && count >= 0) {
+            return true
+            _errorInput.value = false
+        } else {
+            return false
+            _errorInput.value = true
+        }
+    }
+
+    private fun resetErrorInput() {
+        _errorInput.value = false
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
+    }
+}
