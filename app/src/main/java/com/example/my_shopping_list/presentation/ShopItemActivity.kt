1acf1c769894501407fa4207ebbaad26959e10d0
cemaky3 <cemaky3>
2023-08-30 13:09:06 +0300

    AddButton + Intents(factory method)

--- app/src/main/java/com/example/my_shopping_list/presentation/ShopItemActivity.kt
+++ app/src/main/java/com/example/my_shopping_list/presentation/ShopItemActivity.kt
@@ -1,12 +1,38 @@
 package com.example.my_shopping_list.presentation
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.my_shopping_list.R
+import com.example.my_shopping_list.databinding.ActivityShopItemBinding
 
 class ShopItemActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityShopItemBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shop_item)
+        binding = ActivityShopItemBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val mode = intent.getStringExtra("Screen_mode")
+    }
+
+    companion object {
+        private const val SCREEN_MODE = "Screen_mode"
+        private const val SHOP_ITEM_ID = "Shop_item_id"
+        private const val ADD_MODE = "add"
+        private const val EDIT_MODE = "edit"
+
+        fun AddItemIntent(context: Context): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(SCREEN_MODE, ADD_MODE)
+            return intent
+        }
+        fun EditItemIntent(context: Context, shopItemId: Int): Intent {
+            val intent = Intent(context, ShopItemActivity::class.java)
+            intent.putExtra(SCREEN_MODE, EDIT_MODE)
+            intent.putExtra(SHOP_ITEM_ID, shopItemId)
+            return intent
+        }
     }
 }
